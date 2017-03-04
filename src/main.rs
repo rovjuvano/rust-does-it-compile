f68cfd5325af5415845fe4d127713843fc212036
Robert <rovjuvano@gmail.com>
2017-03-03 22:38:43 -0500

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// Use case of lifetimes
+#[derive(Debug)]
+struct Object {
+    value: String,
+}
+#[derive(Debug)]
+struct Wrapper<'a> {
+    object: &'a mut Object,
+}
+impl<'a> Wrapper<'a> {
+    fn bar(&mut self) {
+        self.object.value.push_str("bar");
+    }
+    fn baz(&mut self) {
+        self.object.value.push_str("baz");
+    }
+}
+fn mutate<'a>(object: &'a mut Object) {
+    object.value.push_str("foo");
+}
+fn wrap<'a>(object: &'a mut Object) -> Wrapper<'a> {
+    Wrapper { object: object }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut object = Object { value: "".to_string() };
+    mutate(&mut object); // mutate<'a> begins and ends here
+    {
+        let mut wrapper = wrap(&mut object); // Wrapper<'a> begins here ...
+        wrapper.bar();
+        wrapper.baz();
+        println!("{:?}", wrapper);
+    } // ... Wrapper<'a> ends here
+    println!("{:?}", object);
 }
