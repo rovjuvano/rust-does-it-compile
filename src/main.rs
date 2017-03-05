edf4308d91b15ce29e611c15da6afec87eef782d
Robert Juliano <rovjuvano@gmail.com>
2017-03-05 14:28:53 -0500

    inline original functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let subject = &mut "foo".to_string();
-    (&mut* subject).push_str("bar");
-    (&mut* subject).push_str("baz");
-    (&mut* subject).push_str("quux");
+    let mut subject = "foo".to_string();
+    (&mut subject).push_str("bar");
+    (&mut (&mut subject)).push_str("baz");
+    (&mut (&mut (&mut subject))).push_str("quux");
     println!("subject={:?}", subject);
 }
