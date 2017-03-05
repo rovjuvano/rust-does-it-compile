2231307b8a5fdcbf55d01d74accd6fc24f34a4e8
Robert Juliano <rovjuvano@gmail.com>
2017-03-05 14:06:53 -0500

    make binding immutable in functions

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     bar(subject);
     println!("subject={:?}", subject);
 }
-fn bar(mut string: &mut String) {
+fn bar(string: &mut String) {
     string.push_str("bar");
     baz(string);
 }
-fn baz(mut string: &mut String) {
+fn baz(string: &mut String) {
     string.push_str("baz");
     quux(string);
 }
-fn quux(mut string: &mut String) {
+fn quux(string: &mut String) {
     string.push_str("quux");
 }
