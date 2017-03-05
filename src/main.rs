44f2cf1f3833270e20c3b50494f1fa6fbb687105
Robert Juliano <rovjuvano@gmail.com>
2017-03-05 14:19:32 -0500

    make all bindings immutable

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let mut subject = &mut "foo".to_string();
+    let subject = &mut "foo".to_string();
     {
-        let mut string = &mut subject;
+        let string = &mut* subject;
         string.push_str("bar");
         {
-            let mut string = &mut string;
+            let string = &mut* string;
             string.push_str("baz");
             {
-                let mut string = &mut string;
+                let string = &mut* string;
                 string.push_str("quux");
             }
         }
