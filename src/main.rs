9b842f4e6f23e822ca86b8c6b32d9cdcb17c9d7f
Robert Juliano <rovjuvano@gmail.com>
2017-03-05 14:23:48 -0500

    inline blocks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 fn main() {
     let subject = &mut "foo".to_string();
-    {
-        let string = &mut* subject;
-        string.push_str("bar");
-        {
-            let string = &mut* string;
-            string.push_str("baz");
-            {
-                let string = &mut* string;
-                string.push_str("quux");
-            }
-        }
-    }
+    (&mut* subject).push_str("bar");
+    (&mut* subject).push_str("baz");
+    (&mut* subject).push_str("quux");
     println!("subject={:?}", subject);
 }
