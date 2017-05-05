e2df147e5041f19a19bda38e202ac0908cd068e6
Robert Juliano <rovjuvano@gmail.com>
2017-05-05 16:10:57 -0400

    basic pattern

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+struct MyType {
+    a: String,
+    b: u8,
+    c: Vec<u8>,
+}
 fn main() {
-    println!("Hello, world!");
+    let x = MyType { a: "String".to_string(), b: 8u8, c: vec![1, 2, 3] };
+    match x {
+        MyType { a, b, c } => println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c),
+    };
 }
