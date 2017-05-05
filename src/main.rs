80726e8ae3324f6eb2130c067f9f9c1946131c10
Robert Juliano <rovjuvano@gmail.com>
2017-05-05 16:56:16 -0400

    let pattern ref

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ struct MyType {
 }
 fn main() {
     let x = &MyType { a: "String".to_string(), b: 8u8, c: vec![1, 2, 3] };
-    match x {
-        &MyType { ref a, ref b, ref c } => println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c),
-    };
+    let &MyType { ref a, ref b, ref c } = x;
+    println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c);
 }
