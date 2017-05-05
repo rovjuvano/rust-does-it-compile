8ea6ba7b733df48ce4bb7ec309ebf9e4a7f3c1b4
Robert Juliano <rovjuvano@gmail.com>
2017-05-05 17:03:08 -0400

    basic pattern mut

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ struct MyType {
     c: Vec<u8>,
 }
 fn main() {
-    let x = &mut MyType { a: "String".to_string(), b: 8u8, c: vec![1, 2, 3] };
-    fun(x);
-}
-fn fun(&mut MyType { ref mut a, ref b, ref c }: &mut MyType) {
-    a.push_str(" appended");
-    println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c);
+    let mut x = MyType { a: "String".to_string(), b: 8u8, c: vec![1, 2, 3] };
+    match x {
+        MyType { mut a, b, c } => {
+            a.push_str(" appended");
+            println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c)
+        },
+    };
 }
