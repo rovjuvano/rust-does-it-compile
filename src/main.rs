425ebe2ab0c30ee43ffbd0bea4fcc69eed677d52
Robert Juliano <rovjuvano@gmail.com>
2017-05-21 23:17:55 -0400

    let pattern mut ref

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ struct MyType {
 }
 fn main() {
     let x = &mut MyType { a: "String".to_string(), b: 8u8, c: vec![1, 2, 3] };
-    match x {
-        &mut MyType { ref mut a, ref b, ref c } => {
-            a.push_str(" appended");
-            println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c)
-        },
-    };
+    let &mut MyType { ref mut a, ref b, ref c } = x;
+    a.push_str(" appended");
+    println!("a: {:?}\nb: {:?}\nc: {:?}", a, b, c);
 }
